4bbc2db2edc5d5f78f83aabdd1ef1a198eca68c0
Rafael <55895943+MrPinabutter@users.noreply.github.com>
2020-02-25 02:56:56 -0300

    Update 1905.py

--- URI_Judge/1905.py
+++ URI_Judge/1905.py
@@ -6,18 +6,18 @@ def caminho(lab, cords, ant_cord=[-1, -1]):
     elif a == 4 and b == 4:
         return True
 
-    if a < 4:
+    if a < 4:   # Primeiro é tentado todos para baixo
         if lab[a + 1][b] == '0':
             prox_cord = [a + 1, b]
             return caminho(lab, prox_cord, cords)
 
-    if b < 4:
+    if b < 4:   # Tentado todos á direita
         if lab[a][b + 1] == '0':
             if ant_cord[0] != a or ant_cord[1] != b+1:
                 prox_cord = [a, b + 1]
                 return caminho(lab, prox_cord, cords)
 
-    if b > 0:
+    if b > 0:   # Tentado caminho á esquerda
         if lab[a][b - 1] == '0':
             prox_cord = [a, b - 1]
             return caminho(lab, prox_cord, cords)
