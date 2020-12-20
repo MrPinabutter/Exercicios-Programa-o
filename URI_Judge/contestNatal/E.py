9ea7d8d25f8b8261222374e9b380308d082680f1
MrPinabutter <vitorafael.r.c@gmail.com>
2020-12-20 20:47:16 -0300

    🎅 Xmas

--- URI_Judge/contestNatal/E.py
+++ URI_Judge/contestNatal/E.py
@@ -1,3 +1,17 @@
+def isConectado(arestas, vertice, visitados=[], maxi=0):
+    if vertice not in visitados:
+        visitados.append(vertice)
+    cop = arestas.copy()
+    for vizinho in cop:
+        if vertice in vizinho:
+            if vertice == vizinho[1]:
+                arestas.remove(vizinho)
+                isConectado(cop, vizinho[0], visitados, maxi)
+            else:
+                arestas.remove(vizinho)
+                isConectado(arestas, vizinho[1], visitados)
+    return len(visitados) == maxi 
+
 n, l = input().split(' ')
 
 n = int(n)
@@ -5,26 +19,7 @@ l = int(l)
 
 coords = []
 for i in range(l):
-    x, y = input().split(' ')
-    x = int(x)
-    y = int(y)
-    if (x, y) not in coords and (y, x) not in coords:
-        coords.append((x, y)) 
+    x, y = map(int, input().split(' '))
+    coords.append((x, y)) 
 
-def isConectado(arestas, vertice, visitados=[], maxi=0):
-    visitados.append(vertice)
-    cont = 0
-    ver = True
-    cop = arestas.copy()
-    for vizinho in cop:
-        if vertice in vizinho:
-            cont += 1
-            arestas.remove(vizinho)
-            if vertice == vizinho[1]:
-                isConectado(arestas, vizinho[0], visitados, maxi)
-            else:
-                isConectado(arestas, vizinho[1], visitados)
-        if cont > 2:
-            ver = False
-    return len(visitados) == maxi and ver
-print("COMPLETO" if isConectado(coords.copy(), 1, maxi=maximo) else "INCOMPLETO") 
+print("COMPLETO" if isConectado(coords.copy(), 1, maxi=n) else "INCOMPLETO") 
